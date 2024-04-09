5b87a9904df6bc85252dc4a0b82d445bb284c952
Raman-14 <ramanofficial014@gmail.com>
2024-04-09 18:09:07 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { AccountComponent } from './pages/account/account.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full',
+  },
+  {
+    path:'home',
+    component:HomeComponent,
+    title:'Home-assignment',
+  },
+  {
+    path:'create-account',
+    component:CreateAccountComponent,
+    title:'Create-account',
+  },
+  {
+    path:'account',
+    component:AccountComponent,
+    title:'account',
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    title:'login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
